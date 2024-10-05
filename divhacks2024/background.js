d36f5beb38bdc5f2cc3b6e0d8b5003888d4a0369
emilyta788 <emilyta788@gmail.com>
2024-10-05 14:30:53 -0400

    Update background.js

--- divhacks2024/background.js
+++ divhacks2024/background.js
@@ -18,4 +18,19 @@ function isSocialMedia(url) {
 
   // Check if the domain matches one of the social media sites
   return socialMediaSites.has(domain);
+
+
 }
+
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url.includes('socialMediaSites')) { 
+    chrome.action.openPopup();
+  }
+});
+
+let activeTabId = null;
+let tabStartTime = null;
+
+// Monitors when tab is activated
+chrome.tabs
