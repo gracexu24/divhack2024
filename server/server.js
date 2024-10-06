4d10d46537419eac9c4dd3938b3a6cc4b3780fda
gracexu24 <114009551+gracexu24@users.noreply.github.com>
2024-10-05 22:33:48 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 const cors = require('cors'); 
 
 // Enable CORS for all origins (or specify your frontend origin)
